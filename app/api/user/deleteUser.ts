404d6620304ac00c64939fd3c732d6426a68cfac
Firekiking84 <kgalaxie84@gmail.com>
2025-07-10 14:52:36 +0200

    Passage de l'ancien format de classes au nouveau

--- app/api/user/deleteUser.ts
+++ app/api/user/deleteUser.ts
@@ -1,14 +1,22 @@
-import {Database} from "../../../lib/database";
+import {Database} from "../../../lib/database/database";
 import {NextResponse} from "next/server";
+import {UserEntity} from "../../../lib/database/UserEntity";
+import {status} from "../../../lib/types";
 
 export async function deleteUser(body: {user_id: number | undefined},
                                  caller_id: number): Promise<NextResponse> {
     if (body.user_id === undefined)
         return (NextResponse.json({error: "'user_id' is required!"}, {status: 400}));
-    const database = Database.getInstance();
-    if (caller_id != body.user_id && !(await database.isAdmin(caller_id)).result)
+    const user: UserEntity = new UserEntity();
+    let status: status = await user.fetch(caller_id);
+    if (!status.success)
+        return (NextResponse.json({error: status.error}, {status: 400}))
+    if (caller_id != body.user_id && !(await user.isAdmin()).result)
         return (NextResponse.json({error: "Only admin can delete users!"}, {status: 403}));
-    const status = await database.deleteUser(body.user_id);
+    status = await user.fetch(body.user_id);
+    if (!status.success)
+        return (NextResponse.json({error: status.error}, {status: 400}));
+    status = await user.delete();
     if (!status.success)
         return (NextResponse.json({error: status.error}, {status: 400}));
     return (NextResponse.json({}, {status: 200}));
