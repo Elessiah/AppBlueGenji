1f1704ae9cad2491f34de5b30f54cbb865df6d6d
Firekiking84 <kgalaxie84@gmail.com>
2025-07-10 14:47:45 +0200

    Passage de l'ancien format de classes au nouveau

--- app/api/user/auth.ts
+++ app/api/user/auth.ts
@@ -1,12 +1,15 @@
 import {NextResponse} from "next/server";
-import {Database} from "../../../lib/database";
 import {status} from "../../../lib/types";
+import {UserEntity} from "../../../lib/database/UserEntity";
 
 export async function auth(body: {user: number | string | undefined, password: string | undefined}): Promise<NextResponse> {
     if (body.user === undefined || body.password === undefined)
         return (NextResponse.json({error: "'user' and 'password' are required!"}, {status: 400}));
-    const database = Database.getInstance();
-    const auth: status & {token: string} = await database.authPasswordUser(body.user, body.password);
+    const user: UserEntity = new UserEntity();
+    const status: status = await user.fetch(body.user);
+    if (!status.success)
+        return (NextResponse.json({error: status.error}, {status: 401}));
+    const auth: status & {token: string} = await user.authPassword(body.password);
     if (!auth.success || auth.token.length == 0)
         return NextResponse.json({error: "Wrong password or username"}, {status: 401});
     const response = NextResponse.json({}, {status: 200});
