c42ef296ecc9f0d8b270ced515d813e3ddc066e7
Firekiking84 <kgalaxie84@gmail.com>
2025-07-29 14:06:08 +0200

    Push front

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header/>
+      {/*<Header/>*/}
         <UserProvider><Suspense>{children}</Suspense></UserProvider>
         <Footer/>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
-"use client";
-import {redirect, useSearchParams} from "next/navigation";
+import * as motion from "motion/react-client";
+import "./Home.css";
 
 export default function Home() {
-    const searchParams = useSearchParams();
-    const error: string | null = searchParams.get('error');
-    if (error != null) {
-        redirect("/tournament/list?error=" + error);
-    }
-    redirect("/tournament/list");
+
+    const items = [
+        { src: "/BotPart.png", className: "top", alt: "Bot Part" },
+        { src: "/MarvelRivalsPart.jpg", className: "right", alt: "Marvel Rivals" },
+        { src: "/tournamentPart.jpeg", className: "bottom", alt: "Tournament" },
+        { src: "/OverwatchPart.png", className: "left", alt: "Overwatch" },
+    ];
+
+    return (
+        <div className="triangle-container">
+            {/* Logo central */}
+            <div className="triangle-center">
+                <img src="/logo_bg.webp" alt="BlueGenji" />
+            </div>
+
+            {/* Triangles en cascade */}
+            {items.map((item, index) => (
+                <motion.div
+                    key={item.className}
+                    initial={{ opacity: 0, scale: 0 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ delay: 0.4 + index * 0.3,
+                        type: "spring",
+                        bounce: 0.4,
+                        duration: 0.6, }}
+                >
+                    <img src={item.src} className={`triangle ${item.className}`} alt={item.alt} />
+                </motion.div>
+            ))}
+        </div>
+    );
 }
